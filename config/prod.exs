cac80729cd3bdf7d753044758c1c3b4493ff5f64
Jake Runzer <jakerunzer@gmail.com>
2017-09-27 13:26:26 -0700

    do not force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :writing, WritingWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: System.get_env("DOMAIN"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: System.get_env("DOMAIN"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
