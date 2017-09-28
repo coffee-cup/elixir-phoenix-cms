a7ef0decf8c1724f08ea09c0bfbe602fc152205b
Jake Runzer <jakerunzer@gmail.com>
2017-09-28 15:23:40 -0700

    path changes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,9 +5,11 @@ if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
   echo "Starting deploying 🚀"
 
+  ls
+
   eval "$(ssh-agent -s)"
-  chmod 600 ./travis_dokku
-  ssh-add ./travis_dokku
+  chmod 600 travis_dokku
+  ssh-add travis_dokku
   ssh-keyscan 159.203.47.250 >> ~/.ssh/known_hosts
   git remote add dokku dokku@jakerunzer.space:writing
   git config --global push.default simple
