49f444e9d604e3649c9ba3413a85bce9325cb72e
Jake Runzer <jakerunzer@gmail.com>
2017-10-08 02:56:19 -0600

    add delete functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,8 @@ import '../scss/app.scss';
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-// import 'phoenix_html';
+import phoenixHtml from './phoenix_html';
+phoenixHtml();
 
 // Import local files
 //
