952b77b229d497fcb57d370955f6a1d3dbaa0532
Jake Runzer <jakerunzer@gmail.com>
2017-10-23 23:37:01 -0700

    update og metatags

--- lib/writing_web/views/layout_view.ex
+++ lib/writing_web/views/layout_view.ex
@@ -5,4 +5,6 @@ defmodule WritingWeb.LayoutView do
   def site_url, do: "http://w.jakerunzer.xyz"
   def title, do: "w."
   def description, do: "A collection of articles about things I find interesting."
+
+  # <meta name="description" content="<%= Map.get(assigns, :description, description()) %>">
 end
